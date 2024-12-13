0ebc3dc1cd4548987125debcda888de25c228477
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2024-12-13 18:38:39 +0100

    exercise5

--- exercise5.py
+++ exercise5.py
@@ -5,7 +5,8 @@ def mondayGeneretor(date1, date2):
     finish_date = datetime.strptime(date2, "%d-%m-%Y")
     while start_date <= finish_date:
         if start_date.weekday() == 0:
-            print(start_date.date())
+            result = datetime.strftime(start_date, "%d-%m-%Y")
+            print(result)
         start_date += timedelta(days=1)
 
 mondayGeneretor("13-12-2024", "1-1-2025")
